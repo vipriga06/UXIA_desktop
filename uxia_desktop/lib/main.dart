3edde989455d5b0c70f6dec200aa2b25da6d9cf1
Victor Prieto <victorpg182006@gmail.com>
2026-02-02 22:08:04 +0100

    styles

--- uxia_desktop/lib/main.dart
+++ uxia_desktop/lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uxia_desktop/theme/app_styles.dart';
 
 void main() => runApp(const Aida());
 
@@ -72,7 +74,6 @@ class InputField extends StatelessWidget {
       controller: controller,
       obscureText: obscureText,
       decoration: InputDecoration(
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
         labelText: label,
         prefixIcon: Icon(icon),
       ),
@@ -80,16 +81,59 @@ class InputField extends StatelessWidget {
   }
 }
 
-class LogoHeader extends StatelessWidget {
+class LogoHeader extends StatefulWidget {
   const LogoHeader({super.key});
+
+  @override
+  State<LogoHeader> createState() => _LogoHeaderState();
+}
+
+class _LogoHeaderState extends State<LogoHeader>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutCubic,
+    ));
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final isMobile = MediaQuery.of(context).size.width < 600;
-    return SvgPicture.asset(
-      'assets/logos/logo_completo.svg',
-      height: isMobile ? 100 : 220,
-      fit: BoxFit.contain,
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: SvgPicture.asset(
+          'assets/logos/logo_completo.svg',
+          height: isMobile ? AppSizes.logoHeightMobile : AppSizes.logoHeightDesktop,
+          fit: BoxFit.contain,
+        ),
+      ),
     );
   }
 }
@@ -106,16 +150,7 @@ class Aida extends StatelessWidget {
     return MaterialApp(
       title: 'A.I.D.A administrator',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF8bc6cc),
-          secondary: const Color(0xFF476568),
-          tertiary: const Color(0xFFffffff),
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFffffff),
-      ),
+      theme: AppTheme.light,
       home: const LoginView(),
     );
   }
@@ -132,11 +167,13 @@ class LoginView extends StatefulWidget {
   State<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _LoginViewState extends State<LoginView> with SingleTickerProviderStateMixin {
   late final PreferencesService _preferencesService;
   late final TextEditingController _urlController;
   late final TextEditingController _usuariController;
   late final TextEditingController _contrasenyaController;
+  late AnimationController _blurController;
+  late Animation<double> _blurAnimation;
 
   @override
   void initState() {
@@ -145,11 +182,20 @@ class _LoginViewState extends State<LoginView> {
     _urlController = TextEditingController();
     _usuariController = TextEditingController();
     _contrasenyaController = TextEditingController();
+    _blurController = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+    _blurAnimation = Tween<double>(begin: 0.0, end: 10.0).animate(
+      CurvedAnimation(parent: _blurController, curve: Curves.easeOut),
+    );
     _carregarDades();
+    _blurController.forward();
   }
 
   @override
   void dispose() {
+    _blurController.dispose();
     _urlController.dispose();
     _usuariController.dispose();
     _contrasenyaController.dispose();
@@ -175,14 +221,46 @@ class _LoginViewState extends State<LoginView> {
   }
 
   void _mostrarDialogLogin() {
-    showDialog(
+    showGeneralDialog(
       context: context,
-      builder: (dialogContext) => _LoginDialog(
-        urlController: _urlController,
-        usuariController: _usuariController,
-        contrasenyaController: _contrasenyaController,
-        onEntrar: _guardarDades,
-      ),
+      barrierDismissible: true,
+      barrierLabel: 'Dismiss',
+      barrierColor: Colors.black54,
+      transitionDuration: const Duration(milliseconds: 300),
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return _LoginDialog(
+          urlController: _urlController,
+          usuariController: _usuariController,
+          contrasenyaController: _contrasenyaController,
+          onEntrar: _guardarDades,
+        );
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: Curves.easeOutCubic,
+        );
+        final blurAnimation = Tween<double>(begin: 0.0, end: 20.0).animate(curvedAnimation);
+        return AnimatedBuilder(
+          animation: blurAnimation,
+          builder: (context, dialogChild) {
+            return BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: blurAnimation.value,
+                sigmaY: blurAnimation.value,
+              ),
+              child: ScaleTransition(
+                scale: Tween<double>(begin: 0.8, end: 1.0).animate(curvedAnimation),
+                child: FadeTransition(
+                  opacity: curvedAnimation,
+                  child: dialogChild,
+                ),
+              ),
+            );
+          },
+          child: child,
+        );
+      },
     );
   }
 
@@ -192,33 +270,63 @@ class _LoginViewState extends State<LoginView> {
     final screenWidth = MediaQuery.of(context).size.width;
     
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: isMobile ? 88 : 160,
-        title: const LogoHeader(),
-        centerTitle: true,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          isMobile ? AppSizes.appBarHeightMobile : AppSizes.appBarHeightDesktop,
+        ),
+        child: ClipRect(
+          child: AnimatedBuilder(
+            animation: _blurAnimation,
+            builder: (context, child) {
+              return BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: _blurAnimation.value,
+                  sigmaY: _blurAnimation.value,
+                ),
+                child: child,
+              );
+            },
+            child: AppBar(
+              toolbarHeight: isMobile
+                  ? AppSizes.appBarHeightMobile
+                  : AppSizes.appBarHeightDesktop,
+              title: const LogoHeader(),
+              centerTitle: true,
+              backgroundColor: AppColors.primary.withValues(alpha: 0.85),
+            ),
+          ),
+        ),
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: isMobile ? 16.0 : 32.0,
-              vertical: isMobile ? 24.0 : 32.0,
-            ),
-            child: SizedBox(
-              width: isMobile ? screenWidth - 32 : 280,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(
-                    vertical: isMobile ? 16 : 20,
-                    horizontal: isMobile ? 24 : 32,
-                  ),
+          child: TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0.0, end: 1.0),
+            duration: const Duration(milliseconds: 600),
+            curve: Curves.easeOut,
+            builder: (context, value, child) {
+              return Opacity(
+                opacity: value,
+                child: Transform.translate(
+                  offset: Offset(0, 20 * (1 - value)),
+                  child: child,
                 ),
-                onPressed: _mostrarDialogLogin,
-                child: Text(
-                  'Logejar-se',
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontSize: isMobile ? 16 : 18,
-                    fontWeight: FontWeight.w600,
+              );
+            },
+            child: Padding(
+              padding: AppInsets.screenPadding(isMobile: isMobile),
+              child: SizedBox(
+                width: isMobile ? screenWidth - (AppSpacing.md * 2) : AppSizes.buttonWidthDesktop,
+                child: ElevatedButton(
+                  style: AppButtonStyles.elevated(isMobile: isMobile),
+                  onPressed: _mostrarDialogLogin,
+                  child: Text(
+                    'Logejar-se',
+                    style: AppTextStyles.headlineSmall(
+                      context,
+                      fontSize: isMobile
+                          ? AppSizes.buttonFontMobile
+                          : AppSizes.buttonFontDesktop,
+                    ),
                   ),
                 ),
               ),
@@ -251,13 +359,16 @@ class _LoginDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final isMobile = MediaQuery.of(context).size.width < 600;
 
-    return AlertDialog(
-      title: const Center(child: Text('Inici de sessió')),
-      contentPadding: EdgeInsets.symmetric(
-        horizontal: isMobile ? 16 : 24,
-        vertical: isMobile ? 16 : 20,
-      ),
-      content: SingleChildScrollView(
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(16)),
+      child: AlertDialog(
+        backgroundColor: AppColors.background.withValues(alpha: 0.9),
+        title: const Center(child: Text('Inici de sessió')),
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: isMobile ? 16 : 24,
+          vertical: isMobile ? 16 : 20,
+        ),
+        content: SingleChildScrollView(
         child: ConstrainedBox(
           constraints: BoxConstraints(
             maxWidth: isMobile ? 280 : 400,
@@ -265,23 +376,26 @@ class _LoginDialog extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              InputField(
+              _AnimatedInputField(
                 controller: urlController,
                 label: 'URL del servidor',
                 icon: Icons.cloud,
+                delay: 0,
               ),
               SizedBox(height: isMobile ? 12 : 16),
-              InputField(
+              _AnimatedInputField(
                 controller: usuariController,
                 label: 'Nom d\'usuari',
                 icon: Icons.person,
+                delay: 100,
               ),
               SizedBox(height: isMobile ? 12 : 16),
-              InputField(
+              _AnimatedInputField(
                 controller: contrasenyaController,
                 label: 'Contrasenya',
                 obscureText: true,
                 icon: Icons.lock,
+                delay: 200,
               ),
             ],
           ),
@@ -295,11 +409,85 @@ class _LoginDialog extends StatelessWidget {
         FilledButton(
           onPressed: () async {
             await onEntrar();
-            Navigator.of(context).pop();
+            if (context.mounted) {
+              Navigator.of(context).pop();
+            }
           },
           child: const Text('Entrar'),
         ),
       ],
+        ),
+      );
+  }
+}
+
+class _AnimatedInputField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final bool obscureText;
+  final IconData icon;
+  final int delay;
+
+  const _AnimatedInputField({
+    required this.controller,
+    required this.label,
+    this.obscureText = false,
+    required this.icon,
+    this.delay = 0,
+  });
+
+  @override
+  State<_AnimatedInputField> createState() => _AnimatedInputFieldState();
+}
+
+class _AnimatedInputFieldState extends State<_AnimatedInputField>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutCubic,
+    ));
+    Future.delayed(Duration(milliseconds: widget.delay), () {
+      if (mounted) _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: InputField(
+          controller: widget.controller,
+          label: widget.label,
+          obscureText: widget.obscureText,
+          icon: widget.icon,
+        ),
+      ),
     );
   }
 }
